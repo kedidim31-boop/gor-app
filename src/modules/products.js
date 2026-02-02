3128f4622c8c0af08dd987e6b95ce73a108d055e
kedidim31-boop <kedidim31@gmail.com>
2026-02-02 06:36:39 +0100

    Delete src/modules directory

--- src/modules/products.js
+++
@@ -1,6 +0,0 @@
-import { db } from "../utils/firebase.js";
-import { collection, addDoc } from "firebase/firestore";
-
-export async function addProduct(product) {
-  await addDoc(collection(db, "products"), product);
-}
